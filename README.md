a1e6e40c5ca722eefa44d9499ced2abb87929e6b
Daniel Tomé Fernández <danieltomefer@gmail.com>
2025-02-02 16:39:45 +0100

    Deployed NFT contract and minimal updates

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 ✅ $SMOL ERC-20 contract deployed to:
 `0xb8e5d6eDf418479aA4CC73def6E4DbB2a07E47E9`
 
+✅ $SNFT ERC-721 contract deployed to:
+`0xFe1385fd1d1eAc3aE82D884695EB677523bc4f87`
+
 How to deploy:
 
 ```sh
 npx hardhat run scripts/deploy.ts --network base_sepolia
+```
+
+```sh
+npx hardhat run scripts/deployNft.ts --network base_sepolia
 ```
--- contracts/SmolNFT.sol
+++ contracts/SmolNFT.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.20;
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmolNFT is ERC721, Ownable {
+contract SmolNFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
 
     constructor() ERC721("SmolNFT", "SNFT") Ownable(msg.sender) {}
 
     function mint(address to, string memory tokenURI) external onlyOwner {
         _safeMint(to, nextTokenId);
-        _setTokenURI(nextTokenId, tokenUri);
+        super._setTokenURI(nextTokenId, tokenURI);
         nextTokenId++;
     }
 }
